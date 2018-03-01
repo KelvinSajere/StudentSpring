60b07d8d4c6b76daa3919701dba5e57285fe244d
KelvinSajere <yovwisajere@gmail.com>
2018-03-01 11:54:03 -0700

    Added Student Entity

---
+++ src/main/java/com/studentapp/entity/Student.java
@@ -0,0 +1,43 @@
+package com.studentapp.entity;
+
+
+public class Student {
+	
+	private String name;
+	private int studentID;
+	private String department;
+	
+	
+	/**
+	 * @param name
+	 * @param studentID
+	 * @param department
+	 */
+	
+	public Student(String name, int studentID, String department) {
+		super();
+		this.name = name;
+		this.studentID = studentID;
+		this.department = department;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getStudentID() {
+		return studentID;
+	}
+	public void setStudentID(int studentID) {
+		this.studentID = studentID;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	
+
+}
